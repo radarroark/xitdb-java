7e9f1a22a1d72c3489638cde2b29a7488fbdc27b
radar roark <122068506+radarroark@users.noreply.github.com>
2025-04-15 16:03:25 -0400

    write db header

--- src/main/java/net/haxy/Database.java
+++ src/main/java/net/haxy/Database.java
@@ -1,13 +1,49 @@
 package net.haxy;
 
-public class Database {
-    Kind kind;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
-    public Database(Kind kind) {
-        this.kind = kind;
+public class Database {
+    File file;
+    DatabaseHeader header;
+
+    public Database(File file) throws IOException {
+        this.file = file;
+
+        if (file.length() == 0) {
+            this.header = new DatabaseHeader();
+            this.writeHeader();
+        }
     }
 
-    public enum Kind {
-        FILE
+    private DatabaseHeader writeHeader() throws IOException {
+        var header = new DatabaseHeader();
+        try (var os = new DataOutputStream(new FileOutputStream(this.file))) {
+            os.write(header.getBytes().array());
+        }
+        return header;
+    }
+}
+
+class DatabaseHeader {
+    int hashId = 0; // TODO: don't hardcode this
+    short hashSize = 20; // TODO: don't hardcode this
+    short version = VERSION;
+    byte tag = 0;
+    String magicNumber = "xit";
+
+    public static final short VERSION = 0;
+
+    public ByteBuffer getBytes() {
+        var buffer = ByteBuffer.allocate(96);
+        buffer.put(this.magicNumber.getBytes());
+        buffer.put(this.tag);
+        buffer.putShort(this.version);
+        buffer.putShort(this.hashSize);
+        buffer.putInt(this.hashId);
+        return buffer;
     }
 }
--- src/test/java/net/haxy/DatabaseTest.java
+++ src/test/java/net/haxy/DatabaseTest.java
@@ -4,10 +4,32 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 class DatabaseTest {
     @Test
-    void initDatabase() {
-        var db = new Database(Database.Kind.FILE);
-        assertEquals(Database.Kind.FILE, db.kind);
+    void initDatabase() throws IOException {
+        var file = File.createTempFile("database", "");
+        file.deleteOnExit();
+
+        var db = new Database(file);
+
+        try (var os = new DataInputStream(new FileInputStream(db.file))) {
+            var magicNumber = new byte[3];
+            os.read(magicNumber);
+            var tag = os.readByte();
+            var version = os.readShort();
+            var hashSize = os.readShort();
+            var hashId = os.readInt();
+
+            assertEquals("xit", new String(magicNumber));
+            assertEquals(0, tag);
+            assertEquals(0, version);
+            assertEquals(20, hashSize);
+            assertEquals(0, hashId);
+        }
     }
 }
