d735f0ee7dde1fc9ce095f3485c656db9f2b6dc5
radar roark <122068506+radarroark@users.noreply.github.com>
2025-04-25 11:48:46 -0400

    add project.clj for deploying to clojars

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject io.github.radarroark/xitdb "0.1.0"
+  :url "https://github.com/radarroark/xitdb-java"
+  :description "An immutable database"
+  :license {:name "MIT License"
+            :url "https://github.com/radarroark/xitdb-java/blob/master/LICENSE"}
+  :repositories [["clojars" {:url "https://clojars.org/repo"
+                             :sign-releases false}]]
+  :java-source-paths ["src/main/java"])
