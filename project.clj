915ae5eccb35f202001733f8e51d68afc37de897
radar roark <122068506+radarroark@users.noreply.github.com>
2025-04-26 10:28:04 -0400

    add jar exclusions

--- project.clj
+++ project.clj
@@ -5,4 +5,8 @@
             :url "https://github.com/radarroark/xitdb-java/blob/master/LICENSE"}
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :sign-releases false}]]
+  ;; everything in src is included by default, which causes duplicate
+  ;; files in the jar since the :java-source-paths includes a sub dir.
+  ;; the :jar-exclusions prevents this from happening...
+  :jar-exclusions [#"main/.*" #"test/.*"]
   :java-source-paths ["src/main/java"])
