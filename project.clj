bbc03a656ee4f1c386010bc7bff7e18f9916d3a5
radar roark <122068506+radarroark@users.noreply.github.com>
2025-05-20 06:19:28 -0400

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.radarroark/xitdb "0.12.0"
+(defproject io.github.radarroark/xitdb "0.13.0"
   :url "https://github.com/radarroark/xitdb-java"
   :description "An immutable database"
   :license {:name "MIT License"
