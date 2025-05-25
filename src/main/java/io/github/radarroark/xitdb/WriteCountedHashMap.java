9a5f2b19f98810eee18e95329446199c42cc3e7f
radar roark <122068506+radarroark@users.noreply.github.com>
2025-05-25 08:27:19 -0400

    allow mixing counted maps/sets

--- src/main/java/io/github/radarroark/xitdb/WriteCountedHashMap.java
+++ src/main/java/io/github/radarroark/xitdb/WriteCountedHashMap.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class WriteCountedHashMap extends WriteHashMap {
     public WriteCountedHashMap(WriteCursor cursor) throws Exception {
         switch (cursor.slotPtr.slot().tag()) {
-            case NONE, COUNTED_HASH_MAP -> {
+            case NONE, COUNTED_HASH_MAP, COUNTED_HASH_SET -> {
                 this.cursor = cursor.writePath(new Database.PathPart[]{
                     new Database.HashMapInit(true, false)
                 });
--- src/main/java/io/github/radarroark/xitdb/WriteCountedHashSet.java
+++ src/main/java/io/github/radarroark/xitdb/WriteCountedHashSet.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class WriteCountedHashSet extends WriteHashSet {
     public WriteCountedHashSet(WriteCursor cursor) throws Exception {
         switch (cursor.slotPtr.slot().tag()) {
-            case NONE, COUNTED_HASH_SET -> {
+            case NONE, COUNTED_HASH_MAP, COUNTED_HASH_SET -> {
                 this.cursor = cursor.writePath(new Database.PathPart[]{
                     new Database.HashMapInit(true, true)
                 });
